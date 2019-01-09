04884d7c0e8746a2fe4835e3dbf4cddfd79c88ea
kenny098 <kenny@qq.com>
2019-01-09 15:38:27 +0800

    no message

---
+++ point/echo_color.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+## blue to echo 
+function blue(){
+    echo -e "\033[34m[ $1 ]\033[0m"
+}
+
+
+## green to echo 
+function green(){
+    echo -e "\033[32m[ $1 ]\033[0m"
+}
+
+## Error to warning with blink
+function bred(){
+    echo -e "\033[31m\033[01m\033[05m[ $1 ]\033[0m"
+}
+
+## Error to warning with blink
+function byellow(){
+    echo -e "\033[33m\033[01m\033[05m[ $1 ]\033[0m"
+}
+
+
+## Error
+function bred(){
+    echo -e "\033[31m\033[01m[ $1 ]\033[0m"
+}
+
+## warning
+function byellow(){
+    echo -e "\033[33m\033[01m[ $1 ]\033[0m"
+}
+
+green "this is green"
+blue  "this is blue"
+bred "this is read"
+byellow "this is yellow"
