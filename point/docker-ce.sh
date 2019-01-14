26b63b63f84e55c9a31326a24acbffe4cd4216aa
kenny098 <kenny@qq.com>
2019-01-14 18:23:20 +0800

    no message

--- point/docker-ce.sh
+++ point/docker-ce.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# remove the old version docker
 yum remove docker \
                   docker-client \
                   docker-client-latest \
@@ -11,16 +12,20 @@ yum remove docker \
                   docker-engine-selinux \
                   docker-engine
 
+# install the depence tools
 yum install -y yum-utils \
   device-mapper-persistent-data \
   lvm2
 
+# add the docker-ce repo
 yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
 
+# install the custom docker-ce version	
 yum install -y docker-ce-18.06.1.ce
 
+# start docker and enable the docker service on boot
 systemctl start docker
 
 systemctl enable docker
